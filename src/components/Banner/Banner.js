08873a25837341bce5edf11aa6e60257a9a40907
Leonardo dos Santos <lsantos@filah.com.br>
2022-12-05 22:00:27 -0300

    clean projetc

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="acessibilidadde"/>
+        </header>
+    )
+}
+
+export default Banner
